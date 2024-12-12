ce3b30f37d35d17abe4982eca6cca55183007218
Anurag-Tarai <anuragtarai051@gmail.com>
2024-12-12 20:26:17 +0530

    working in messages

--- src/main/java/com/tarai/project_management_system_backend/controller/MessageController.java
+++ src/main/java/com/tarai/project_management_system_backend/controller/MessageController.java
@@ -2,28 +2,65 @@ package com.tarai.project_management_system_backend.controller;
 
 import com.tarai.project_management_system_backend.entity.Message;
 import com.tarai.project_management_system_backend.request.MessageRequest;
+import com.tarai.project_management_system_backend.response.MessageResponse;
 import com.tarai.project_management_system_backend.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/message")
 public class MessageController {
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
     @Autowired
-    MessageService messageService;
+    private MessageService messageService;
 
     @PostMapping("/send")
-    public ResponseEntity<MessageRequest>sendMessage(@RequestBody MessageRequest messageRequest) throws Exception {
-        messageService.sendMessage(messageRequest.getSenderId(), messageRequest.getProjectId(),messageRequest.getContent());
-        return new ResponseEntity<>(messageRequest, HttpStatus.OK);
+    public ResponseEntity<MessageResponse> sendMessage(@RequestBody MessageRequest messageRequest) {
+        try {
+            // Log the incoming request
+            logger.info("Sending message from user: {} to project: {}", messageRequest.getSenderId(), messageRequest.getProjectId());
+
+            // Call service method to send message
+            messageService.sendMessage(messageRequest.getSenderId(), messageRequest.getProjectId(), messageRequest.getContent());
+
+            // Create response object
+            MessageResponse response = new MessageResponse("Message sent successfully", messageRequest);
+
+            return ResponseEntity.ok(response);  // Return a more informative response
+
+        } catch (Exception e) {
+            // Log the exception
+            logger.error("Error sending message", e);
+            return ResponseEntity.status(500).body(new MessageResponse("Failed to send message", null));
+        }
     }
 
     @GetMapping("/chat/{projectId}")
-    public ResponseEntity<List<Message>> getMessagesByChat(@PathVariable Long projectId) throws Exception {
-        return  new ResponseEntity<>(messageService.getMessagesByProjectId(projectId), HttpStatus.OK);
+    public ResponseEntity<List<Message>> getMessagesByChat(@PathVariable Long projectId) {
+        try {
+            // Log the incoming request
+            logger.info("Fetching messages for project: {}", projectId);
+
+            // Get messages from service
+            List<Message> messages = messageService.getMessagesByProjectId(projectId);
+
+            if (messages.isEmpty()) {
+                logger.info("No messages found for project: {}", projectId);
+                return ResponseEntity.noContent().build(); // Return no content if no messages are found
+            }
+
+            return ResponseEntity.ok(messages);
+
+        } catch (Exception e) {
+            // Log the exception
+            logger.error("Error fetching messages for project: {}", projectId, e);
+            return ResponseEntity.status(500).build(); // Return a generic server error response
+        }
     }
 }
--- src/main/java/com/tarai/project_management_system_backend/response/MessageResponse.java
+++ src/main/java/com/tarai/project_management_system_backend/response/MessageResponse.java
@@ -1,5 +1,6 @@
 package com.tarai.project_management_system_backend.response;
 
+import com.tarai.project_management_system_backend.request.MessageRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,4 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MessageResponse {
     private String message;
+    private MessageRequest messageRequest;
+
+    // Constructor for only a message
+    public MessageResponse(String message) {
+        this.message = message;
+    }
 }
